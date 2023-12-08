e9ff5ea7c52db37e27a54cc5736b528d0791bff7
Martijn van der Blom <martijn@teamofteams.nl>
2023-12-08 13:37:44 +0100

    Initial commit

---
+++ src/main/kotlin/dev/vanderblom/intergamma/sideeffects/SideeffectsApplication.kt
@@ -0,0 +1,11 @@
+package dev.vanderblom.intergamma.sideeffects
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SideeffectsApplication
+
+fun main(args: Array<String>) {
+	runApplication<SideeffectsApplication>(*args)
+}
---
+++ src/test/kotlin/dev/vanderblom/intergamma/sideeffects/SideeffectsApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.vanderblom.intergamma.sideeffects
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SideeffectsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
